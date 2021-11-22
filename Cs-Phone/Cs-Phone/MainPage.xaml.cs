2282eee14e80249bb2e94b316de5601f43889378
Dawid <bilinskidawid5@gmail.com>
2021-11-22 20:56:59 +0000

    Made a generic button/circle, added a tap gesture so that it displays an alert when pressed

---
+++ Cs-Phone/Cs-Phone/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Cs_Phone
+{
+    [DesignTimeVisible(true)]
+    public partial class MainPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+
+            PanGestureRecognizer = new PanGestureRecognizer
+            {
+                TouchPoints = 1
+            };
+            PanGestureRecognizer.PanUpdated += PanGestureRecognizer_PanUpdated;
+            
+        }
+
+        public PanGestureRecognizer PanGestureRecognizer { get; set; }
+
+        private void PanGestureRecognizer_PanUpdated(object sender, PanUpdatedEventArgs e)
+        {
+
+            StatusLabel.Text = e.StatusType.ToString(); // Canceled, Completed, Running, Started
+            GestureIdLabel.Text = e.GestureId.ToString();
+            TotalXLabel.Text = Math.Round(e.TotalX, 1).ToString(CultureInfo.InvariantCulture);
+            TotalYLabel.Text = Math.Round(e.TotalY, 1).ToString(CultureInfo.InvariantCulture);
+        }
+    }
+}
---
+++ Nea-Phone/Nea-Phone/MainPage.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using System.Diagnostics;
+
+namespace Nea_Phone
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            mouseClick(); //once the program starts, you can click the button
+        }
+
+        void mouseClick()
+        {
+            theMouse.GestureRecognizers.Add(new TapGestureRecognizer()
+            { Command = new Command(() => { DisplayAlert("Task", "Clicked", "Ok"); }) });
+
+        }
+
+    }
+
+
+}
